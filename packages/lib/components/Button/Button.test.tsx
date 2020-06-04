c6a1b23994b3519132418674c5bf11d1e831abc3
Pedro Filho <pedro@filho.me>
2020-06-04 13:53:05 +0100

    Fixing configs and test

--- packages/lib/components/Button/Button.test.tsx
+++ packages/lib/components/Button/Button.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Button from ".";
 
-test("adds 1 + 2 to equal 3", () => {
+test("Renders Button", () => {
 	const { container } = render(<Button>Something</Button>);
 
 	expect(container).toBeInTheDocument();
