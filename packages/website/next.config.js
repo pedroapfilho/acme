c0dd103f194f2f214a2b2daaff83ed376caad35b
Pedro Filho <pedro@filho.me>
2020-07-27 13:49:28 +0100

    Deleting website (unnecessary for boilerplate)

--- packages/website/next.config.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withTM = require("next-transpile-modules")(["@acme/lib"]);
-
-module.exports = withTM();
--- packages/website/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { NextPage } from "next";
-import { Button } from "@acme/lib";
-
-const Home: NextPage = () => {
-	return <Button>Website</Button>;
-};
-
-export default Home;
